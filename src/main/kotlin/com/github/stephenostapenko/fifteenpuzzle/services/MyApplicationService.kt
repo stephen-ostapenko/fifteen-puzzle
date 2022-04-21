67f812e9242a04eb36ba5afb4278407cfb2b4a23
GitHub Action <action@github.com>
2022-04-21 18:23:20 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/stephenostapenko/fifteenpuzzle/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.stephenostapenko.fifteenpuzzle.services
+
+import com.github.stephenostapenko.fifteenpuzzle.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/stephenostapenko/fifteenpuzzle/services/MyProjectService.kt
+++ src/main/kotlin/com/github/stephenostapenko/fifteenpuzzle/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.stephenostapenko.fifteenpuzzle.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stephenostapenko.fifteenpuzzle.MyBundle
 
 class MyProjectService(project: Project) {
 
