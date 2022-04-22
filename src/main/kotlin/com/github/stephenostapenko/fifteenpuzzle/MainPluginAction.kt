913b6459043c5973ce9705e3dcae00f8ea28371d
Stephen Ostapenko <stepanos2002@gmail.com>
2022-04-22 16:58:43 +0300

    setting up the project

---
+++ src/main/kotlin/com/github/stephenostapenko/fifteenpuzzle/MainPluginAction.kt
@@ -0,0 +1,10 @@
+package com.github.stephenostapenko.fifteenpuzzle
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
+
+class MainPluginAction : DumbAwareAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO()
+    }
+}
---
+++ src/main/kotlin/com/github/stephenostapenko/fifteenpuzzle/WindowFactory.kt
@@ -0,0 +1,12 @@
+package com.github.stephenostapenko.fifteenpuzzle
+
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+
+class WindowFactory : ToolWindowFactory, DumbAware {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        TODO()
+    }
+}
