2f6dec5bb9be9dc1116e482b515d3ec7cd8c6078
Stephen Ostapenko <stepanos2002@gmail.com>
2022-05-04 16:29:30 +0300

    add action

--- src/main/kotlin/com/github/stephenostapenko/fifteenpuzzle/MainPluginAction.kt
+++ src/main/kotlin/com/github/stephenostapenko/fifteenpuzzle/MainPluginAction.kt
@@ -2,9 +2,11 @@ package com.github.stephenostapenko.fifteenpuzzle
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.wm.ToolWindowManager
 
 class MainPluginAction : DumbAwareAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        TODO()
+        val p = e.project ?: return
+        ToolWindowManager.getInstance(p).getToolWindow("Fifteen Puzzle")?.show()
     }
 }
